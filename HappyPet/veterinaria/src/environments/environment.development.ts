79a70adf2483fd497da251ff461660bb056ea631
Italo González Muñoz <ita.gonzalez@duocuc.cl>
2025-05-05 11:16:05 -0400

    Cambios puerto BD

--- HappyPet/veterinaria/src/environments/environment.development.ts
+++ HappyPet/veterinaria/src/environments/environment.development.ts
@@ -1,9 +1,9 @@
 export const environment = {
   version: '1.0.0',
   baseUrl: 'http://172.17.17.29:8080/veterinariabackend/#',
-  baseAuthenticationUrl: 'http://localhost:8900/veterinariabackend/api',
-  baseUrlBackend: 'http://localhost:8900/veterinariabackend/api',
-  //baseUrlBackend: 'http://172.17.17.29:8080/skynetbackend',
+  baseAuthenticationUrl: 'http://localhost:8080/veterinariabackend/api',
+  baseUrlBackend: 'http://localhost:8080/veterinariabackend/api',
+  //baseUrlBackend: 'http://172.17.17.29:8080/',
 
   security: {
     secretKey: 'U2FsdGVkX18f0Vl+x9f5Hm1s2TEBv6VKVy2AxiXf3PgK2xpcOMtOtPe6yFyfYYbH'
